82d46878e2a00c1042f965827f5b3375e620f5d0
jm2498 <jm2498@cornell.edu>
2015-12-02 22:02:36 -0500

    distribute algorithm works now

--- CVCalendar Demo/ViewController.swift
+++ CVCalendar Demo/ViewController.swift
@@ -32,13 +32,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     var animationFinished = true
     
     let eventStore = EKEventStore()
-    let calendar = NSCalendar.currentCalendar()
 	let dateFormatter = NSDateFormatter()
     var selectedDay: CVCalendarDayView!
 	var eventDays = [CVCalendarDayView]()
 	var workHours = [Float]()
 	var today: CVCalendarDayView!
 	var predict: NSPredicate!
+	var numOfDays: Int!
+	var numOfWeeks: Int!
+	var totalHoursToWork: Float!
     
     // MARK: - Life cycle
     
@@ -48,10 +50,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         monthLabel.text = CVDate(date: NSDate()).globalDescription
         
         if (EKEventStore.authorizationStatusForEntityType(.Event) != EKAuthorizationStatus.Authorized) {
-            eventStore.requestAccessToEntityType(.Event, completion: {granted, error in})
+            self.eventStore.requestAccessToEntityType(.Event, completion: {granted, error in})
         }
         
-        selectedDay = calendarView.coordinator.selectedDayView
+        self.selectedDay = calendarView.coordinator.selectedDayView
 		today = calendarView.coordinator.selectedDayView
 		
     }
@@ -90,17 +92,17 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 	}
 	
 	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return selectedDay.eventList.count
+		return self.selectedDay.eventList.count
 	}
 	
 	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 		
 		let cell = tableView.dequeueReusableCellWithIdentifier("EventTableViewCell", forIndexPath: indexPath)
 		
-		let event = selectedDay.eventList[indexPath.row]
-		dateFormatter.dateFormat = "HH:mm"
-		let start = dateFormatter.stringFromDate(event.startDate)
-		let end = dateFormatter.stringFromDate(event.endDate)
+		let event = self.selectedDay.eventList[indexPath.row]
+		self.dateFormatter.dateFormat = "HH:mm"
+		let start = self.dateFormatter.stringFromDate(event.startDate)
+		let end = self.dateFormatter.stringFromDate(event.endDate)
 		let title = event.title
 		cell.textLabel!.text = "\(start) - \(end) : \(title)"
 		
@@ -136,7 +138,7 @@ extension ViewController: CVCalendarViewDelegate, CVCalendarMenuViewDelegate {
     // MARK: Optional methods
     
     func shouldShowWeekdaysOut() -> Bool {
-        return shouldShowDaysOut
+        return self.shouldShowDaysOut
     }
     
     func shouldAnimateResizing() -> Bool {
@@ -144,7 +146,7 @@ extension ViewController: CVCalendarViewDelegate, CVCalendarMenuViewDelegate {
     }
     
     func didSelectDayView(dayView: CVCalendarDayView) {
-        selectedDay = dayView
+        self.selectedDay = dayView
 		eventTabel.reloadData()
         print("\(dayView.date.commonDescription) is selected!")
 //		predict = eventStore.predicateForEventsWithStartDate(NSDate(), endDate: NSDate(), calendars: nil)
@@ -322,24 +324,24 @@ extension ViewController {
     }
     
     /// Switch to WeekView mode.
-    @IBAction func toWeekView(sender: AnyObject) {
-        calendarView.changeMode(.WeekView)
-    }
-    
-    /// Switch to MonthView mode.
-    @IBAction func toMonthView(sender: AnyObject) {
-        calendarView.changeMode(.MonthView)
-    }
-    
-    @IBAction func loadPrevious(sender: AnyObject) {
-        calendarView.loadPreviousView()
-    }
-    
-    
-    @IBAction func loadNext(sender: AnyObject) {
-        calendarView.loadNextView()
-    }
-    
+//    @IBAction func toWeekView(sender: AnyObject) {
+//        calendarView.changeMode(.WeekView)
+//    }
+//    
+//    /// Switch to MonthView mode.
+//    @IBAction func toMonthView(sender: AnyObject) {
+//        calendarView.changeMode(.MonthView)
+//    }
+//    
+//    @IBAction func loadPrevious(sender: AnyObject) {
+//        calendarView.loadPreviousView()
+//    }
+//    
+//    
+//    @IBAction func loadNext(sender: AnyObject) {
+//        calendarView.loadNextView()
+//    }
+	
     @IBAction func setEventInfo() {
         self.setupViews()
     }
@@ -352,7 +354,7 @@ extension ViewController {
     
     @IBAction func addEvent() {
         self.setupViews()
-		let event = EKEvent(eventStore: eventStore)
+		let event = EKEvent(eventStore: self.eventStore)
 		if (eventTitle.text! == "") {
 			event.title = "New Event"
 		} else {
@@ -373,14 +375,26 @@ extension ViewController {
     }
 	
 	@IBAction func changeWorkLoad(sender: UISlider) {
-		let start = today.date.getDay
-		dateFormatter.dateFormat = "d"
-		let end = Int(dateFormatter.stringFromDate(endDate.date))!
-		let numberOfDays = end - start + 1
-		//let numberOfWeeks =
-		today.setupWorkLoadMarker(CGFloat(sender.value/100*50))
-		for index in 0...numberOfDays-1 {
-			today.weekView.dayViews[today.weekdayIndex+index].setupWorkLoadMarker(CGFloat(sender.value/100*50))
+		self.workHours.removeAll()
+		let day = ceil(self.timeLineBar.value)
+		let up = (sender.value * Float(self.numOfDays) - self.totalHoursToWork) * 2.0
+		let bot1 = day * (day - 1)
+		let bot2 = (Float(self.numOfDays) - day + 1.0) * (Float(self.numOfDays) - day)
+		let delta = up / (bot1 + bot2)
+		print(sender.value)
+		print(self.timeLineBar.value)
+		print(day)
+		for index in 0...(eventDays.count-1) {
+			if (index <= Int(day)-1) {
+				let workload = sender.value-Float(Int(day)-(index+1)) * delta
+				eventDays[index].setupWorkLoadMarker(CGFloat(workload*5.0))
+				workHours.append(workload * 5)
+			}
+			else {
+				let workload = sender.value-Float(index+1-Int(day)) * delta
+				eventDays[index].setupWorkLoadMarker(CGFloat(workload*5.0))
+				workHours.append(workload * 5)
+			}
 		}
 	}
 	
@@ -389,51 +403,91 @@ extension ViewController {
 	}
 	
 	@IBAction func changeEndDate(sender: UIDatePicker) {
-		dateFormatter.dateFormat = "MM-dd"
-		let todayDate = dateFormatter.stringFromDate(NSDate())
-		let datePickerDate = dateFormatter.stringFromDate(sender.date)
+		self.dateFormatter.dateFormat = "MM-dd"
+		let todayDate = self.dateFormatter.stringFromDate(NSDate())
+		let datePickerDate = self.dateFormatter.stringFromDate(sender.date)
 		if (todayDate != datePickerDate) {
+			self.eventDays.removeAll()
 			workLoadBar.enabled = true
 			timeLineBar.enabled = true
+			let start = today.date.getDay
+			self.dateFormatter.dateFormat = "d"
+			let end = Int(self.dateFormatter.stringFromDate(sender.date))!
+			self.numOfDays = end - start + 1
+			self.numOfWeeks = (self.numOfDays-(7-today.weekdayIndex+1))/7
+			if ((self.numOfDays-(7-today.weekdayIndex+1))%7 > 0) {
+				self.numOfWeeks = self.numOfWeeks + 1
+			}
+			self.totalHoursToWork = Float(self.eventDuration.text!)
+			self.timeLineBar.maximumValue = Float(self.numOfDays + 1)
+			self.timeLineBar.value = Float(self.numOfDays) / 2.0
+			self.workLoadBar.maximumValue = 10.0
+			self.workLoadBar.value = self.totalHoursToWork / Float(self.numOfDays)
+			print(numOfDays)
+			
+			for index in 0...(7-today.weekdayIndex) {
+				eventDays.append(today.weekView.dayViews[today.weekdayIndex + index - 1])
+			}
+			if (numOfWeeks > 0) {
+				if (self.numOfWeeks == 1) {
+					for index in 0...((self.numOfDays-(7-today.weekdayIndex+1))%7-1) {
+						eventDays.append(today.weekView.monthView.weekViews[today.weekView.index+numOfWeeks].dayViews[index])
+					}
+				}
+				else {
+					for index in 1...self.numOfWeeks {
+						for day in today.weekView.monthView.weekViews[today.weekView.index+index].dayViews {
+							eventDays.append(day)
+						}
+					}
+				}
+				if ((self.numOfDays-(7-today.weekdayIndex+1))%7 > 0 && self.numOfWeeks > 1){
+					for index in 0...((self.numOfDays-(7-today.weekdayIndex+1))%7-1) {
+						eventDays.append(today.weekView.monthView.weekViews[today.weekView.index+numOfWeeks].dayViews[index])
+					}
+				}
+			}
+			for index in 0...(eventDays.count-1) {
+				print(eventDays[index].date.commonDescription)
+			}
 		}
 	}
-	
 	
 	
 }
 
 // MARK: - Convenience API Demo
 
-extension ViewController {
-    func toggleMonthViewWithMonthOffset(offset: Int) {
-        let calendar = NSCalendar.currentCalendar()
-        let calendarManager = calendarView.manager
-        let components = Manager.componentsForDate(NSDate()) // from today
-        
-        components.month += offset
-        
-        let resultDate = calendar.dateFromComponents(components)!
-        
-        self.calendarView.toggleViewWithDate(resultDate)
-    }
-    
-    func didShowNextMonthView(date: NSDate)
-    {
-        let calendar = NSCalendar.currentCalendar()
-        let calendarManager = calendarView.manager
-        let components = Manager.componentsForDate(date) // from today
-        
-        print("Showing Month: \(components.month)")
-    }
-    
-    
-    func didShowPreviousMonthView(date: NSDate)
-    {
-        let calendar = NSCalendar.currentCalendar()
-        let calendarManager = calendarView.manager
-        let components = Manager.componentsForDate(date) // from today
-        
-        print("Showing Month: \(components.month)")
-    }
-    
-}
+//extension ViewController {
+//    func toggleMonthViewWithMonthOffset(offset: Int) {
+//        let calendar = NSCalendar.currentCalendar()
+//        let calendarManager = calendarView.manager
+//        let components = Manager.componentsForDate(NSDate()) // from today
+//        
+//        components.month += offset
+//        
+//        let resultDate = calendar.dateFromComponents(components)!
+//        
+//        self.calendarView.toggleViewWithDate(resultDate)
+//    }
+//    
+//    func didShowNextMonthView(date: NSDate)
+//    {
+//        let calendar = NSCalendar.currentCalendar()
+//        let calendarManager = calendarView.manager
+//        let components = Manager.componentsForDate(date) // from today
+//        
+//        print("Showing Month: \(components.month)")
+//    }
+//    
+//    
+//    func didShowPreviousMonthView(date: NSDate)
+//    {
+//        let calendar = NSCalendar.currentCalendar()
+//        let calendarManager = calendarView.manager
+//        let components = Manager.componentsForDate(date) // from today
+//        
+//        print("Showing Month: \(components.month)")
+//    }
+//    
+//}
